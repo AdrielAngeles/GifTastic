adbae6671c4a5c307109c383a701fa57601f65d5
Adriel Angeles <adrielangeles0107@gmail.com>
2019-09-27 07:36:24 -0700

    first draft

---
+++ GIF.js
@@ -0,0 +1,13 @@
+$(document).ready();
+
+
+        $.ajax({
+            url: "api.giphy.com/v1/gifs/search",
+            apikey: I9GePYDDV7x00wimCxkMmCfoOx9OcwI1 
+           
+            
+            success: function( result ) {
+              $().html(  );
+            }
+          });
+
